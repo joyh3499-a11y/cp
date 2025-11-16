7907b2c6cf386a96ff5b3c835322d03107741d63
Joy Halder <joyh3499@gmail.com>
2025-11-16 06:02:26 +0600

    Add files via upload

---
+++ 136A.cpp
@@ -0,0 +1,18 @@
+/* A. Presents */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int>p(n+1),ans(n+1);
+    for (int i = 1; i <= n; i++) {
+        cin >> p[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        ans[p[i]]=i;
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+}
