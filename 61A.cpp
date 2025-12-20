19356016074fa439fdf349375d43c73a04c0028c
Joy Halder <joyh3499@gmail.com>
2025-12-20 21:45:42 +0600

    Add files via upload

---
+++ 61A.cpp
@@ -0,0 +1,14 @@
+// A. Ultra-Fast Mathematician //
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++) {
+        cout << (a[i] != b[i] ? '1' : '0');
+    }
+    cout << endl;
+    return 0;
+}
