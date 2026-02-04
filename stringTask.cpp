b7f28b881dc363a28a599b97338ac43942dab26e
Joy Halder <joyh3499@gmail.com>
2026-02-04 19:36:50 +0600

    Update string processing logic in stringTask.cpp
    Refactor string processing to filter out vowels and format output.

--- stringTask.cpp
+++ stringTask.cpp
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    string n;
-    cin >> n;
 
-  int up =0;
-    int low = 0;
+int main()
+{
+    string s;
+    cin >> s;
 
-    for (char c:n) {
-        if (isupper(c)) up++;
-        else low++;
+    for (char c : s)
+    {
+        char lower = tolower(c);
+
+
+        if (lower != 'a' && lower != 'o' && lower != 'y' &&
+                lower != 'e' && lower != 'u' && lower != 'i')
+        {
+            cout << "." << lower;
+        }
     }
-    if (up > low) {
-        for (char &c : n) c = toupper(c);
-    } else {
-        for (char &c : n) c = tolower(c);
-    }
 
-    cout << n << endl;
     return 0;
-
 }
